8dc15d1a9bd8ec4a1fca470c54c5eb2e83a2d9eb
MattiaSusin <m.susin28@gmail.com>
2024-06-25 17:12:41 +0200

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 import TopBar from "./components/TopBar";
 import MyFooter from "./components/MyFooter";
+import MyAlert from "./components/MyAlert";
 
 
 function App() {
   return (
     <div className="App">
       <TopBar/>
-      <MyFooter className=""/>
+      <MyAlert/>
+      <MyFooter/>
       
     </div>
   );
---
+++ src/components/MyAlert.jsx
@@ -0,0 +1,8 @@
+import { Alert, AlertHeading } from "react-bootstrap";
+
+const MyAlert = props => (
+    <Alert className="ms-5 me-5">
+        <AlertHeading>!!!Benvenuto nel nostro Book Shop!!!</AlertHeading>
+    </Alert>
+) 
+export default MyAlert;
