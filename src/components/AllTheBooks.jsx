6d456c1ff0463c9b972bb16fd0d04964f3a5f922
MattiaSusin <m.susin28@gmail.com>
2024-06-25 17:46:34 +0200

    Update AllTheBooks.jsx

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -2,12 +2,6 @@ import { Component } from "react";
 
 class AllTheBooks extends Component {
 
-    state = {
-        book:0,
-    };
-    render(){
-        <div>
-            <h1></h1>
-        </div>
-    }
+    
+
 }
